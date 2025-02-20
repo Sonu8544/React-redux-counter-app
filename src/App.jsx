18f2c10d9642c8cf8309062f93fa69336a7734a0
Sonu Kumar <sksonu8544@gmail.com>
2025-02-20 11:44:54 +0530

    RTK with counter App

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <h1 class="text-3xl font-bold text-center text-blue-500 mt-20">
+      Hello world!
+    </h1>
+  )
+}
+
+export default App
